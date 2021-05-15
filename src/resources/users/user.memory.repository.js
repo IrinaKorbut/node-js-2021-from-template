197e49ed42410e8fa8193c9dc6f392fc4af36f6e
IrinaKorbut <irisharomka96@gmail.com>
2021-05-15 14:22:51 +0200

    refactor: refactor user's resources

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,18 +1,17 @@
 const User = require("./user.model");
-
-const userRepo = []
+const userRepo = require("../../common/dataBaseInMemory/userDB");
 
 const getAll = async () => userRepo;
-
-const create = async (userData) => {
-  const user = new User(userData);
-  userRepo.push(user);
-  return user;
-}
 
 const get = async (id) => {
   const targetUsers = userRepo.filter(user => user.id === id);
   return targetUsers[0];
+}
+
+const create = async (userData) => {
+  const user = new User(userData);
+  userRepo.push(user);
+  return get(user.id);
 }
 
 const remove = async (id) => {
@@ -21,12 +20,12 @@ const remove = async (id) => {
     if (user.id === id) {
       userIndex = index;
     }
-  })
+  });
   userRepo.splice(userIndex, 1);
 }
 
 const update = async (id, user) => {
-  const oldUser = await get(id)
+  const oldUser = await get(id);
   userRepo[userRepo.indexOf(oldUser)] = { ...oldUser, ...user, id };
   return get(id);
 }
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,9 +1,13 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+
 const create = (userData) => usersRepo.create(userData);
+
 const get = (id) => usersRepo.get(id);
+
 const remove = (id) => usersRepo.remove(id);
+
 const update = (id, userData) => usersRepo.update(id, userData);
 
 module.exports = { 
