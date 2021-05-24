f83afc6302d336ee3798fd78d7f06a4b30bb318c
IrinaKorbut <irisharomka96@gmail.com>
2021-05-24 15:35:46 +0200

    docs: documenting functions

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,25 +1,50 @@
 const User = require("./user.model");
 const userDB = require("../../common/dataBaseInMemory/userDB");
 
+/**
+ * Get all users
+ * @returns {array<object>} Array of user's objects
+ */
 const getAll = async () => userDB;
 
+/**
+ * Get user by id
+ * @param {string} id - User's id
+ * @returns {object} Object of user
+ */
 const get = async (id) => {
   const targetUsers = userDB.find(user => user.id === id);
   return targetUsers;
 }
 
+/**
+ * Create user
+ * @param {object} userData - Object of user's data
+ * @returns {object} Object of user
+ */
 const create = async (userData) => {
   const user = new User(userData);
   userDB.push(user);
   return get(user.id);
 }
 
+/**
+ * Remove user by id
+ * @param {string} id - User's id
+ * @returns {object} Object of user or null
+ */
 const remove = async (id) => {
   const board = await get(id);
   const userIndex = userDB.indexOf(board);
   return userIndex > -1 ? userDB.splice(userIndex, 1) : null;
 }
 
+/**
+ * Update user by id
+ * @param {string} id - User's id
+ * @param {object} userData - Object of user's data
+ * @returns {object} Object of user
+ */
 const update = async (id, user) => {
   const oldUser = await get(id);
   userDB[userDB.indexOf(oldUser)] = { ...oldUser, ...user, id };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,17 +1,42 @@
 const usersRepo = require('./user.memory.repository');
 const tasksRepo = require('../tasks/task.memory.repository');
 
+/**
+ * Get all users
+ * @returns {array<object>} Array of user's objects
+ */
 const getAll = () => usersRepo.getAll();
 
+/**
+ * Create user
+ * @param {object} userData - Object of user's data
+ * @returns {object} Object of user
+ */
 const create = (userData) => usersRepo.create(userData);
 
+/**
+ * Get user by id
+ * @param {string} id - User's id
+ * @returns {object} Object of user
+ */
 const get = (id) => usersRepo.get(id);
 
+/**
+ * Remove user by id
+ * @param {string} id - User's id
+ * @returns {object} Object of user or null
+ */
 const remove = (id) => {
     tasksRepo.updateTasksAfterDeletingUser(id);
     return usersRepo.remove(id)
 };
 
+/**
+ * Update user by id
+ * @param {string} id - User's id
+ * @param {object} userData - Object of user's data
+ * @returns {object} Object of user
+ */
 const update = (id, userData) => usersRepo.update(id, userData);
 
 module.exports = { 
