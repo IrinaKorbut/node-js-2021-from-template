b00a633aa6a1eef4736d4f1c6c2786987a5b61aa
IrinaKorbut <irisharomka96@gmail.com>
2021-06-24 22:40:23 +0200

    feat: add db

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import app from './app';
 import { config } from './common/config';
+import { TryDBConnect } from './common/helpers/db'
 
 const { PORT } = config;
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+const startConnection = async () => {
+  try {
+    await TryDBConnect();
+    console.log('Successfully connected!')
+  } catch (err) {
+    console.error('error server', err);
+  }
+  app.listen(config.PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`),
+  );
+};
+startConnection();
+
