e39b88a92b2d521d4fb4d42ffd4ed362d38768b9
IrinaKorbut <irisharomka96@gmail.com>
2021-06-27 19:25:43 +0200

    fix: remove

--- src/resources/users/user.service.ts
+++ src/resources/users/user.service.ts
@@ -1,6 +1,6 @@
 import { usersRepo } from './user.memory.repository';
 import tasksRepo from '../tasks/task.memory.repository';
-import { IUser, UserDTO } from '../../types/index';
+import { IUser, UserDTO } from '../../types';
 
 const getAll = (): Promise<Omit <IUser[], 'password'>> => usersRepo.getAll();
 
@@ -8,8 +8,8 @@ const create = (userData: IUser): Promise<IUser> => usersRepo.create(userData);
 
 const get = (id: string): Promise<IUser | 'NOT_FOUND'> => usersRepo.get(id);
 
-const remove = (id: string): Promise<'NOT_FOUND' | 'DELETED'> => {
-  tasksRepo.updateTasksAfterDeletingUser(id);
+const remove = async (id: string): Promise<'NOT_FOUND' | 'DELETED'> => {
+  await tasksRepo.updateTasksAfterDeletingUser(id);
   return usersRepo.remove(id);
 };
 
